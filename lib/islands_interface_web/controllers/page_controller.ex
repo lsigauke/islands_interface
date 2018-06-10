62386679ac2330845b93d60a3440b33f8703b6e2
Lee Sigauke <leesigs@gmail.com>
2018-06-10 12:16:53 +0100

    Add API to channel for interacting with the game server

--- lib/islands_interface_web/controllers/page_controller.ex
+++ lib/islands_interface_web/controllers/page_controller.ex
@@ -3,11 +3,12 @@ defmodule IslandsInterfaceWeb.PageController do
   alias IslandsEngine.GameSupervisor
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render(conn, "index.html")
   end
 
   def test(conn, %{"name" => name}) do
     {:ok, _pid} = GameSupervisor.start_game(name)
+
     conn
     |> put_flash(:info, "You entered the name: #{name}")
     |> render("index.html")
